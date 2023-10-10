fc58a8b9cd0ad13c7f101c83ccf4881858bca31e
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-10-10 15:15:21 +0200

    add cards of hotels

--- index.php
+++ index.php
@@ -45,9 +45,34 @@ $hotels = [
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
    <title>Hotel</title>
 </head>
-<body>
+<body class="bg-dark text-white">
+
+   <h1 class="text-center my-3">Hotels</h1>
+   <div class="row row-cols-5 mx-4">
+
+   <?php foreach ($hotels as $hotel) : ?>
+      <div class="col">
+         <div class="card h-100">
+            <div class="card-body">
+               <h5 class="card-title"><?= $hotel["name"] ?></h5>
+               <p class="card-text"><?= $hotel["description"] ?></p>
+               <p class="card-text">Parking:
+                  <?php if ($hotel["parking"] != true) {
+                     echo "NOT";
+                  }?>
+                  avaiable
+               </p>
+               <p class="card-text">Vote: <?= $hotel["vote"] ?></p>
+               <p class="card-text">Distance to center: <?= $hotel["distance_to_center"] ?> chilometers</p>
+            </div>
+         </div>
+      </div>
+   <?php endforeach ?>
+
+   </div>
    
 </body>
 </html>
